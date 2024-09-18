ea7060e08c300c71d92c267e9f626d4607718024
Elijah McDonald <elijahmc@protonmail.com>
2024-09-18 15:23:28 +1000

    Javascript - Is object empty

---
+++ JavaScript/Is Object Empty.js
@@ -0,0 +1,16 @@
+// https://leetcode.com/submissions/detail/1393993556/
+
+/**
+ * @param {Object|Array} obj
+ * @return {boolean}
+ */
+var isEmpty = function(obj) {
+    if(Object.keys(obj).length === 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+};
